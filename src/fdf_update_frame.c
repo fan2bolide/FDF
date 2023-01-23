2f0a319c5b64a8c32a5e88745ee3f62122c19fcf
Basile Jeannot <basiljeannot@gmail.com>
2023-01-23 21:03:21 +0100

    changed keys to zoom

--- src/fdf_update_frame.c
+++ src/fdf_update_frame.c
@@ -6,7 +6,7 @@
 /*   By: bajeanno <bajeanno@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 19:56:26 by bajeanno          #+#    #+#             */
-/*   Updated: 2023/01/23 20:45:40 by bajeanno         ###   ########lyon.fr   */
+/*   Updated: 2023/01/23 21:02:43 by bajeanno         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@ static void fdf_reset_map(t_fdf *fdf)
 int	fdf_update_frame(int keycode, t_fdf *fdf)
 {
 	fdf_draw_lines(fdf, 0x000000);
-	if (keycode == PLUS_KEY)
+	if (keycode == 38)
 		fdf->map->scale *= 1.1;
-	else
+	if (keycode == 40)
 		fdf->map->scale *= 0.9;
 	ft_printf("updated scale : %d !\n", fdf->map->scale);
 	fdf_reset_map(fdf);
